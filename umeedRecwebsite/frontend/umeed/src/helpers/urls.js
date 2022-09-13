69436ea5bbdf3868031c22986dcee122ba4fac3c
Kuldeep Yadav <85286229+kuldeeprec@users.noreply.github.com>
2022-09-13 17:19:09 +0530

    de

--- umeedRecwebsite/frontend/umeed/src/helpers/urls.js
+++
@@ -1,8 +0,0 @@
-const API_ROOT = "https://umeedbackend.herokuapp.com/api/v1";
-
-export const APIUrls = {
-  login: () => `${API_ROOT}/user/create-session`,
-  signup: () => `${API_ROOT}/users/signup`,
-  fetchStudent: (rollno) => `${API_ROOT}/student/info/?rollno=${rollno}`,
-  fetcheducator: (batch) => `${API_ROOT}/teacher/info/?batch=${batch}`,
-};
