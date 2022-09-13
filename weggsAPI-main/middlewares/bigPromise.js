409465b628c927c8dad7b28ecb8cf412f1b43cd4
Kuldeep Yadav <85286229+kuldeeprec@users.noreply.github.com>
2022-09-13 17:19:44 +0530

    Delete weggsAPI-main directory

--- weggsAPI-main/middlewares/bigPromise.js
+++
@@ -1,5 +0,0 @@
-//using promise because it is better than using try-catch or async-await
-
-module.exports = (func) => (req, res, next) => {
-  Promise.resolve(func(req, res, next)).catch(next);
-};
